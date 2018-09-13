d724590c4802e9ef64221e933c6a959fc0446d8c
XWxiaowei <2809641033@qq.com>
2018-09-13 14:11:06 +0800

    搭建项目基础

---
+++ jay_blog/src/main/java/com/jay/jay_blog/JayBlogApplication.java
@@ -0,0 +1,12 @@
+package com.jay.jay_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JayBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JayBlogApplication.class, args);
+    }
+}
