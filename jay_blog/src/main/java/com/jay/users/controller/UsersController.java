c8be90864a3816f8f9d5acb65c38420b2f54d14f
XWxiaowei <2809641033@qq.com>
2018-09-19 23:43:43 +0800

    集成mybatis-plus

---
+++ jay_blog/src/main/java/com/jay/users/controller/UsersController.java
@@ -0,0 +1,21 @@
+package com.jay.users.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author jay.xiang
+ * @since 2018-09-19
+ */
+@Controller
+@RequestMapping("/users")
+public class UsersController {
+
+}
+
---
+++ jay_blog/src/main/java/com/jay/users/service/UsersService.java
@@ -0,0 +1,16 @@
+package com.jay.Users.service;
+
+import com.jay.model.Users;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author jay.xiang
+ * @since 2018-09-19
+ */
+public interface UsersService extends IService<Users> {
+
+}
